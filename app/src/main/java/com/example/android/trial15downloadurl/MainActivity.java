4e243f3af818af44940a4bb50062686969b4fb31
toneymathews <toneymathews@gmail.com>
2016-10-08 11:09:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/trial15downloadurl/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.android.trial15downloadurl;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection httpURLConnection = null;
+            try {
+                url = new URL(strings[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                int data = inputStreamReader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+                return result;
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Failed";
+
+            }
+
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+        try {
+            result = task.execute("http://google.com").get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.i("Result", result);
+
+
+    }
+}
